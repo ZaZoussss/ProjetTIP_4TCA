6e337e5733052b5a2f88b28e80a2e2f19272f757
f4jov <gabin.fleury@insa-lyon.fr>
2024-11-17 00:22:19 +0100

    Update test_v0.py
    45% accuracy, bad validation, needs to be improved

--- test_v0.py
+++ test_v0.py
@@ -7,6 +7,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
 
 
 import pathlib
@@ -30,7 +31,8 @@ images_width = 64
 # PIL.Image.open(str(cats[0]))
 # print(cats[0])
 
-batch_size = 32
+batch_size = 50
+v_batch_size = 20
 
 # Creating training dataset
 train_ds = tf.keras.utils.image_dataset_from_directory(
@@ -50,7 +52,7 @@ validation_ds = tf.keras.utils.image_dataset_from_directory(
     validation_data_dir,
     color_mode='rgb',
     # batch_size=train_image_count,
-    batch_size=batch_size,
+    batch_size=v_batch_size,
     image_size=(images_height, images_width),
     shuffle=True,
     seed=None,
@@ -65,7 +67,7 @@ print(class_names)
 # Configuring dataset for performance
 AUTOTUNE = tf.data.AUTOTUNE
 
-train_ds = train_ds.prefetch(buffer_size=AUTOTUNE)
+train_ds = train_ds.shuffle(batch_size*2).prefetch(buffer_size=AUTOTUNE)
 validation_ds = validation_ds.prefetch(buffer_size=AUTOTUNE)
 
 # train_ds = train_ds.batch(batch_size).cache(r"L:\big_file_storage\4TCA_S1_TIP\cache\train.cache").shuffle(1000).prefetch(buffer_size=AUTOTUNE)
@@ -75,19 +77,38 @@ validation_ds = validation_ds.prefetch(buffer_size=AUTOTUNE)
 num_classes = len(class_names) # number of classes
 model = Sequential([
   layers.Rescaling(1./255, input_shape=(images_height, images_width, 3)), # changing RGB values from [0, 255] to [0, 1]
-  layers.Conv2D(16, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(64, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Flatten(),
+  
+  layers.Conv2D(8, 3, padding='same', activation='relu'),
+  # layers.Conv2D(15, 3, padding='same', activation='relu'), # not that good
+  # layers.Conv2D(16, 6, padding='same', activation='relu'), # really bad for validation
+  layers.BatchNormalization(),
+  layers.MaxPooling2D(pool_size=(2, 2), strides=2),
+  layers.ReLU(),
+  layers.MaxPool2D(),
+
+  layers.Conv2D(16, 5, padding='same', activation='relu'),
+  #layers.Conv2D(16, 5, padding='same', activation='relu'),
+  layers.BatchNormalization(),
+  layers.MaxPooling2D(pool_size=(2, 2), strides=2),
+  layers.ReLU(),
+  layers.MaxPool2D(),
+
+  # Transformer la sortie 4D en 3D
+  layers.Reshape((-1, 16)),
+
+  layers.SimpleRNN(units=100, recurrent_initializer='RandomNormal'),
+  # layers.SimpleRNN(units=10),
+  layers.Softmax(),
+
   layers.Dense(128, activation='relu'),
   layers.Dense(num_classes)
 ])
 
+# Creating opimizer
+optimize = Adam(learning_rate=0.001)
+
 # Compiling model
-model.compile(optimizer='adam',
+model.compile(optimizer=optimize,
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
@@ -96,7 +117,7 @@ model.compile(optimizer='adam',
 
 
 # Model training
-epochs=10
+epochs=20
 history = model.fit(
   train_ds,
   validation_data=validation_ds,
